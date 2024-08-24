983f840abffd0e7605356b071adbacabfb1648eb
mzio <mzhang2016@gmail.com>
2024-08-23 22:20:35 -0700

    Add old hybrid sliding window from march; refactor lm eval + chungus eval

--- lm_eval_harness/models.py
+++ lm_eval_harness/models.py
@@ -8,6 +8,8 @@ from src.model.modeling_mistral import LolcatsMistralForCausalLM as LOLCATS_MIST
 from src.model.modeling_llama import LooooolcatsLlamaForCausalLM as LOOOOOLCATS_LLAMA_MODEL_CLASS
 from src.model.modeling_mistral import LooooolcatsMistralForCausalLM as LOOOOOLCATS_MISTRAL_MODEL_CLASS
 
+from src.model.modeling_llama_sharded import ShardedLolcatsLlamaForCausalLM as SHARDED_LOLCATS_LLAMA_MODEL_CLASS
+
 
 class LolcatsLlamaForCausalLM(AutoCausalLM):
     """
@@ -32,6 +34,24 @@ class LolcatsMistralForCausalLM(AutoCausalLM):
     Wrapper for Mistral-like autoregressive language model
     """
     AUTO_MODEL_CLASS = LOLCATS_MISTRAL_MODEL_CLASS
+    @property
+    def add_special_tokens(self) -> bool:
+        """Whether to include special tokens in encoded text. This should be
+        determined by whether or not the model was trained with special tokens.
+        TODO: Remove these conditionals once HuggingFace supports a way to
+        check whether or not an arbitrary model was trained with special tokens.
+        """
+        if self._add_special_tokens is not None:
+            return self._add_special_tokens
+        else:
+            return False
+
+
+class ShardedLolcatsLlamaForCausalLM(AutoCausalLM):
+    """
+    Wrapper for Llama or Mistral-like autoregressive language model
+    """
+    AUTO_MODEL_CLASS = SHARDED_LOLCATS_LLAMA_MODEL_CLASS
     @property
     def add_special_tokens(self) -> bool:
         """Whether to include special tokens in encoded text. This should be
